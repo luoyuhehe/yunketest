827d2507013a68110268b063a2b6c241f69f8209
luoyusnnu <2482840559@qq.com>
2020-10-07 00:25:53 +0800

    修改model时间格式

--- db/model.go
+++ db/model.go
@@ -1,20 +1,18 @@
 package db
 
-import "time"
-
 type BaseModel struct {
-	ID         int64     `gorm:"column:id;primary_key"`
-	CreateTime time.Time `gorm:"column:create_time;"`
-	CreateBy   string    `gorm:"column:create_by;"`
-	ModifyTime time.Time `gorm:"column:modify_time;"`
-	ModifyBy   string    `gorm:"column:modify_by;"`
-	DeleteTime time.Time `gorm:"column:delete_time;"`
-	DeleteBy   string    `gorm:"column:delete_by;"`
-	IsDelete   uint8     `gorm:"column:is_delete;"`
+	ID         int64  `gorm:"column:id;primary_key"`
+	CreateTime int64  `gorm:"column:create_time;"`
+	CreateBy   string `gorm:"column:create_by;"`
+	ModifyTime int64  `gorm:"column:modify_time;"`
+	ModifyBy   string `gorm:"column:modify_by;"`
+	DeleteTime int64  `gorm:"column:delete_time;"`
+	DeleteBy   string `gorm:"column:delete_by;"`
+	IsDelete   uint8  `gorm:"column:is_delete;"`
 }
 
 type SimpleBaseModel struct {
-	ID         int64     `gorm:"column:id;primary_key"`
-	CreateTime time.Time `gorm:"column:create_time;"`
-	CreateBy   string    `gorm:"column:create_by;"`
+	ID         int64  `gorm:"column:id;primary_key"`
+	CreateTime int64  `gorm:"column:create_time;"`
+	CreateBy   string `gorm:"column:create_by;"`
 }
